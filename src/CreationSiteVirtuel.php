9211528b24630d191cc5964c56c89cb10ffb9da1
stephane888 <kksasteph888@gmail.com>
2022-06-09 20:54:56 +0200

    MAJ

--- src/CreationSiteVirtuel.php
+++ src/CreationSiteVirtuel.php
@@ -8,4 +8,13 @@ class CreationSiteVirtuel {
     return \Drupal::currentUser()->id();
   }
   
+  public static function getActiveDomain() {
+    /**
+     *
+     * @var \Drupal\domain\DomainNegotiator $domainNegos
+     */
+    $domainNegos = \Drupal::service('domain.negotiator');
+    return $domainNegos->getActiveId();
+  }
+  
 }
