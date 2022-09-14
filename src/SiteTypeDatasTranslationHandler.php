329cc90f6a4721168374205e9d09f7f0d31a36e4
kouwa stephane <kksasteph888@gmail.com>
2022-09-14 23:16:48 +0200

    MAJ3

--- src/SiteTypeDatasTranslationHandler.php
+++ src/SiteTypeDatasTranslationHandler.php
@@ -8,6 +8,6 @@ use Drupal\content_translation\ContentTranslationHandler;
  * Defines the translation handler for site_type_datas.
  */
 class SiteTypeDatasTranslationHandler extends ContentTranslationHandler {
-
+  
   // Override here the needed methods from ContentTranslationHandler.
 }
