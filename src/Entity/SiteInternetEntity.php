f2c93279f9c64f53547c5aed90e11e4123ab233a
kouwa stephane <kksasteph888@gmail.com>
2023-03-09 12:39:12 +0100

    Maj

--- src/Entity/SiteInternetEntity.php
+++ src/Entity/SiteInternetEntity.php
@@ -232,7 +232,7 @@ class SiteInternetEntity extends EditorialContentEntityBase implements SiteInter
     ])->setDisplayOptions('form', [
       'type' => 'string_textfield',
       'weight' => -4
-    ])->setDisplayConfigurable('form', TRUE)->setDisplayConfigurable('view', TRUE)->setRequired(TRUE)->setTranslatable(TRUE);
+    ])->setDisplayConfigurable('form', TRUE)->setDisplayConfigurable('view', TRUE)->setRequired(TRUE)->setTranslatable(true);
     //
     $fields['layout_paragraphs'] = BaseFieldDefinition::create('entity_reference')->setLabel(t(' Sections '))->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED)->setDisplayOptions('form', [
       'type' => 'inline_entity_form_complex',
