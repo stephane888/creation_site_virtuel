acf8c1f05faa3d28ca961035baf20453a9634309
kouwa stephane <kksasteph888@gmail.com>
2024-12-30 10:23:09 +0100

    Maj

--- src/HbkCollectionAccessControlHandler.php
+++ src/HbkCollectionAccessControlHandler.php
@@ -13,43 +13,44 @@ use Drupal\Core\Access\AccessResult;
  * @see \Drupal\creation_site_virtuel\Entity\HbkCollection.
  */
 class HbkCollectionAccessControlHandler extends EntityAccessControlHandler {
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     /** @var \Drupal\creation_site_virtuel\Entity\HbkCollectionInterface $entity */
-
     switch ($operation) {
-
+      
       case 'view':
-
+        
         if (!$entity->isPublished()) {
           return AccessResult::allowedIfHasPermission($account, 'view unpublished collection by habeuk entities');
         }
-
-
-        return AccessResult::allowedIfHasPermission($account, 'view published collection by habeuk entities');
-
+        /**
+         * Pour l'instant on autorise tout le monde à voir les contenus publiés.
+         * On verra apres pour les ameliorations.
+         */
+        return AccessResult::allowed();
+      
       case 'update':
-
+        
         return AccessResult::allowedIfHasPermission($account, 'edit collection by habeuk entities');
-
+      
       case 'delete':
-
+        
         return AccessResult::allowedIfHasPermission($account, 'delete collection by habeuk entities');
     }
-
+    
     // Unknown operation, no opinion.
     return AccessResult::neutral();
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
     return AccessResult::allowedIfHasPermission($account, 'add collection by habeuk entities');
   }
-
-
 }
